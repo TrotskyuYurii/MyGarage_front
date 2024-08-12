ad1298e212dd70007172f8f45b6b1e72269000b4
y.trotskiy <143190183+TrotskyuYurii@users.noreply.github.com>
2024-08-12 21:40:35 +0300

    add route

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
+import { Routes, Route } from "react-router-dom";
+
 import Login from './components/login/Login'
+import WelcomePage from './components/welcomePage/WelcomePage'
+import NotFoundPage from './components/notFoundPage/NotFoundPage'
 import './App.css'
 
 function App() {
 
 
   return (
-    <>
-      <Login />
-    </>
+    <Routes>
+      <Route path="/" element={<WelcomePage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
   )
 }
 
