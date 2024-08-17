ab214e6701e46d569ca09fef5a7b2ce4f87576f9
y.trotskiy <143190183+TrotskyuYurii@users.noreply.github.com>
2024-08-17 12:20:04 +0300

    PigPong test

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,18 @@
 import css from "./home.module.css"
 
+
 const Home = () => {
+
+  const PingPong = () => {
+    console.log("PingPong")
+  }
+
+
   return (
-    <div className={css.wrapper}>Home</div>
+    <div className={css.wrapper}>
+      <div>Home</div>
+      <button className={css.button} onClick={PingPong}>PingPong</button>
+      </div>
   )
 }
 
